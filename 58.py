dbba191ecbb4f605ff410c551c34b317451f03c2
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-05-28 14:46:54 +0530

    Create 58.py

---
+++ 58.py
@@ -0,0 +1,16 @@
+#PATTERN 58
+
+size= 3
+for x in range(size,-(size+1),-1):
+    for y in range(abs(x), size+1):
+        print(chr(y+65),end="")
+    print()
+    
+''' 
+D
+CD 
+BCD 
+ABCD 
+CD
+D 
+'''
